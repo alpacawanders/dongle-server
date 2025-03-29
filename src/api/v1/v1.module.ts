0633ab4af327f244cccd868d8c03cb0d58527b9f
seokimun <gimun08@naver.com>
2025-03-29 18:34:51 +0900

    feat: 🔧 카데고리 리소스 파일 추가

--- src/api/v1/v1.module.ts
+++ src/api/v1/v1.module.ts
@@ -4,8 +4,16 @@ import { NoticeModule } from './notice/notice.module';
 import { ReportModule } from './report/report.module';
 import { BannerModule } from './banner/banner.module';
 import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-    imports: [ClubModule, NoticeModule, ReportModule, BannerModule, UserModule],
+    imports: [
+        ClubModule,
+        NoticeModule,
+        ReportModule,
+        BannerModule,
+        UserModule,
+        CategoryModule,
+    ],
 })
 export class V1Module {}
