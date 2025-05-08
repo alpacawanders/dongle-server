86bea1c211f065e5e257c3e38ff1ea67d48981dd
seokimun <gimun08@naver.com>
2025-05-08 20:24:35 +0900

    feat: ✨ filter 구현 - 사용자 권한체크 필터 추가 - 쿼리 실패 필터 추가

--- src/api/v1/v1.module.ts
+++ src/api/v1/v1.module.ts
@@ -7,6 +7,9 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AuthGuard } from './auth/guard/auth.guard';
 import { RBACGuard } from './auth/guard/rbac.guard';
+import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
+import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
+import { QueryFailedExceptionFilter } from './common/filter/query-failed.filter';
 
 @Module({
     imports: [
@@ -25,6 +28,18 @@ import { RBACGuard } from './auth/guard/rbac.guard';
         {
             provide: 'APP_GUARD',
             useClass: RBACGuard,
+        },
+        {
+            provide: 'APP_INTERCEPTOR',
+            useClass: ResponseTimeInterceptor,
+        },
+        {
+            provide: 'APP_FILTER',
+            useClass: ForbiddenExceptionFilter,
+        },
+        {
+            provide: 'APP_FILTER',
+            useClass: QueryFailedExceptionFilter,
         },
     ],
 })
